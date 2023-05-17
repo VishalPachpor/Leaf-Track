21e0035e3685b443381edf9f251509ce483eac1c
VishalPachpor <Vishalpatil080502@gmail.com>
2023-05-17 20:44:58 +0530

    Optimize OwnershipTransferred event handling

--- leaftrack-subgraph/leaftrack-subgraph/src/leaftrack.ts
+++ leaftrack-subgraph/leaftrack-subgraph/src/leaftrack.ts
@@ -1,76 +1,84 @@
-import {
-  Approval as ApprovalEvent,
-  ApprovalForAll as ApprovalForAllEvent,
-  OwnershipTransferred as OwnershipTransferredEvent,
-  Transfer as TransferEvent
-} from "../generated/leaftrack/leaftrack"
-
-import {
-  Approval,
-  ApprovalForAll,
-  OwnershipTransferred,
-  Transfer
-} from "../generated/schema"
-
+// Approval event handler
 export function handleApproval(event: ApprovalEvent): void {
+  // Create a new Approval entity
   let entity = new Approval(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.approved = event.params.approved
-  entity.tokenId = event.params.tokenId
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // Populate entity properties from the event
+  entity.owner = event.params.owner;
+  entity.approved = event.params.approved;
+  entity.tokenId = event.params.tokenId;
 
-  entity.save()
+  // Populate block-related properties
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  // Save the entity
+  entity.save();
 }
 
+// ApprovalForAll event handler
 export function handleApprovalForAll(event: ApprovalForAllEvent): void {
+  // Create a new ApprovalForAll entity
   let entity = new ApprovalForAll(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.operator = event.params.operator
-  entity.approved = event.params.approved
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // Populate entity properties from the event
+  entity.owner = event.params.owner;
+  entity.operator = event.params.operator;
+  entity.approved = event.params.approved;
 
-  entity.save()
+  // Populate block-related properties
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  // Save the entity
+  entity.save();
 }
 
+// OwnershipTransferred event handler
 export function handleOwnershipTransferred(
   event: OwnershipTransferredEvent
 ): void {
+  // Create a new OwnershipTransferred entity
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.from
-  entity.newOwner = event.params.to
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // Populate entity properties from the event
+  entity.previousOwner = event.params.from;
+  entity.newOwner = event.params.to;
 
-  entity.save()
+  // Populate block-related properties
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  // Save the entity
+  entity.save();
 }
 
-
-
+// Transfer event handler
 export function handleTransfer(event: TransferEvent): void {
+  // Create a new Transfer entity
   let entity = new Transfer(
     event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.from = event.params.from
-  entity.to = event.params.to
-  entity.tokenId = event.params.tokenId
+  );
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  // Populate entity properties from the event
+  entity.from = event.params.from;
+  entity.to = event.params.to;
+  entity.tokenId = event.params.tokenId;
 
-  entity.save()
+  // Populate block-related properties
+  entity.blockNumber = event.block.number;
+  entity.blockTimestamp = event.block.timestamp;
+  entity.transactionHash = event.transaction.hash;
+
+  // Save the entity
+  entity.save();
 }
