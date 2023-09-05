4a71f08f80ffb46b38c2423be2a5c9d1db22f29d
VishalPachpor <Vishalpatil080502@gmail.com>
2023-09-05 21:23:43 +0530

    render details of the protected area including name, ID, and project information

--- src/components/LeafTrackProtectedAreaDetails/index.js
+++ src/components/LeafTrackProtectedAreaDetails/index.js
@@ -14,37 +14,49 @@ export function LeafTrackProtectedAreaDetails() {
   const [error, setError] = React.useState(false);
   const [html, setHtml] = React.useState("");
 
+  // Fetch HTML content from a file
   useEffect(() => {
     fetch("/file.html")
       .then((res) => res.text())
-      .then((text) => { setHtml(text) });
+      .then((text) => {
+        setHtml(text);
+      });
   }, []);
 
+  // Find the protected area based on the provided slug
   const protectedArea = protectedAreasList.find(
     (protectedArea) => protectedArea.id === slug
   );
 
+  // Redirect to home page if user is not connected and a protected area is found
   if (auth.user.walletAddress === "CONNECT WALLET" && protectedArea) {
     return <Navigate to="/" />;
   }
 
   return (
     <div className="details-container">
+      {/* Back button to navigate to the monitoring page */}
       <div className="details-container__back">
         <button onClick={() => navigate("/monitoreo")}>Back</button>
       </div>
+
+      {/* Display details of the protected area */}
       <div className="details-container-head">
         <p className="details-container-head__title">
           Nombre: {protectedArea.name}
         </p>
         <p className="details-container-head__slogan">ID: {protectedArea.id}</p>
       </div>
+
+      {/* Vector divider */}
       <div className="details-container__vector"></div>
+
+      {/* Display project information */}
       <div className="details-container-project">
         <figure>
           <img src={protectedArea.imgbase64} alt="logo" />
         </figure>
-        <div >
+        <div>
           <p className="details-container-project__item">
             Latitud: {protectedArea.lat}
           </p>
@@ -56,14 +68,30 @@ export function LeafTrackProtectedAreaDetails() {
           </p>
         </div>
       </div>
+
+      {/* Display summary information using LeafTrackResumen component */}
       <LeafTrackResumen protectedArea={protectedArea} />
+
+      {/* Display statistics using LeafTrackProtectedAreaStatistics component */}
       <LeafTrackProtectedAreaStatistics
         data={protectedArea.mean}
         labels={"NDVI"}
       />
 
-      {html && <iframe className="iframe" style={{height:"50vh",width:"50vw", display:"flex", textAlign:"center", justifyContent:"center"}} srcdoc={html}></iframe>}
+      {/* Display HTML content in an iframe if available */}
+      {html && (
+        <iframe
+          className="iframe"
+          style={{
+            height: "50vh",
+            width: "50vw",
+            display: "flex",
+            textAlign: "center",
+            justifyContent: "center",
+          }}
+          srcdoc={html}
+        ></iframe>
+      )}
     </div>
-
   );
 }
