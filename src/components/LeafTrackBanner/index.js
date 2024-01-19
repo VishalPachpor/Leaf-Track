5e6cbdd205a5976512de7014dbb7b2aceffd3e13
VishalPachpor <Vishalpatil080502@gmail.com>
2024-01-19 20:56:38 +0530

    This component enhances the modularity of the LeafTrack application, providing a visually appealing way to showcase banners with flexibility in content.

--- src/components/LeafTrackBanner/index.js
+++ src/components/LeafTrackBanner/index.js
@@ -1,10 +1,16 @@
-import "./LeafTrackBanner.scss"
-import React from 'react'
+// Import the styles for LeafTrackBanner component
+import "./LeafTrackBanner.scss";
+import React from "react";
 
-export function LeafTrackBanner({banner}) {
+// LeafTrackBanner component that takes a 'banner' prop
+export function LeafTrackBanner({ banner }) {
   return (
-    <figure className='collection__banner'>
-      <img src={banner} alt='banner' />
+    // 'figure' element with the class 'collection__banner'
+    <figure className="collection__banner">
+      {/* 'img' element to display the banner */}
+      {/* 'src' attribute set to the value of the 'banner' prop */}
+      {/* 'alt' attribute set to 'banner' */}
+      <img src={banner} alt="banner" />
     </figure>
-  )
+  );
 }
